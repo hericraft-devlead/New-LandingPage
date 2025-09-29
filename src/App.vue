7fa99199e1155d898998e6dcddfb8fff7447ab99
Soporte Técnico de Educación a Distancia <103531425+soporteead@users.noreply.github.com>
2025-09-29 13:50:58 -0600

    tailwind install

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -14,6 +20,11 @@ import TheWelcome from './components/TheWelcome.vue'
 
   <main>
     <TheWelcome />
+    <div
+      class="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-500 to-pink-500"
+    >
+      <h1 class="text-5xl font-bold text-white">Tailwind funcionando 💜</h1>
+    </div>
   </main>
 </template>
 
